fd6d937565b2fb97d8253ab0e9abc0232648e980
IDStack <idstack@outlook.co.id>
2020-03-21 10:58:45 +0700

    create post service route

---
+++ src/services/PostService.js
@@ -0,0 +1,34 @@
+import http from "../http";
+
+class PostService {
+    getAll() {
+        return http.get("/posts");
+    }
+
+    get(id) {
+        return http.get(`/posts/${id}`);
+    }
+
+    create(data) {
+        return http.post("/posts", data);
+    }
+
+    update(id, data) {
+        return http.put(`/posts/${id}`, data);
+    }
+
+    delete(id) {
+        return http.delete(`/posts/${id}`);
+    }
+
+    deleteAll() {
+        return http.delete(`/posts`);
+    }
+
+    findByTitle(title) {
+        return http.get(`/posts?title=${title}`);
+    }
+
+}
+
+export default new PostService;
